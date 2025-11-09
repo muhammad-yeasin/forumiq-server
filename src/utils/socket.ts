3419c7fcf99ccc04cb0282db2e969455cb9fdd26
muhammad-yeasin <76746216+muhammad-yeasin@users.noreply.github.com>
2025-11-09 22:32:45 +0600

    fix: socket disabled for testing deploy issue

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,16 +1,16 @@
-import { io } from '@/index'
+// import { io } from '@/index'
 import { IPostWithChildren } from '@/modules/posts/posts.interface'
 
 export const emitNewPost = (threadId: string, post: IPostWithChildren) => {
-    io.to(`thread:${threadId}`).emit('new-post', post)
+    // io.to(`thread:${threadId}`).emit('new-post', post)
 }
 
 export const emitNewNotification = (userId: string, notification: any) => {
-    io.to(`user:${userId}`).emit('new-notification', notification)
+    // io.to(`user:${userId}`).emit('new-notification', notification)
 }
 
 export const emitUpdatedPost = (threadId: string, post: IPostWithChildren) => {
-    io.to(`thread:${threadId}`).emit('update-post', post)
+    // io.to(`thread:${threadId}`).emit('update-post', post)
 }
 
 export const socketService = {
